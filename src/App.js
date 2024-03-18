9f5caffaa8426cf99778b4869da9d9c524305580
knox <42452349+u2knox@users.noreply.github.com>
2024-03-18 18:59:54 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      title: "iphone",
+      price: 1000,
+    },
+    {
+      title: "ipod",
+      price: 600,
+    },
+    {
+      title: "imac",
+      price: 1200,
+    },
+  ]);
+
+  const setItemAmount = (index, value) => {
+    const copyItems = [...items];
+    copyItems[index].count = value;
+    setItems(copyItems);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {items.map((item, index) => (
+        <div key={item.title}>
+          {item.title}: {item.price}
+          {!item.count ? (
+            <button onClick={() => setItemAmount(index, 1)}>Купить</button>
+          ) : (
+            <div>
+              <button onClick={() => setItemAmount(index, item.count - 1)}>-</button>
+              <b>{item.count}</b>
+              <button onClick={() => setItemAmount(index, item.count + 1)}>+</button>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
